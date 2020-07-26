bc7c099ad20e377ce700310286df38d2eb801ac3
GhostClock <zhangyuanlaifen@gmail.com>
2020-07-26 23:23:32 +0800

    OpenGL ES学习:使用GLKit加载图片

---
+++ OpenGL ES/OpenGL_ES_002-使用GLKit加载图片/OpenGL_ES_002/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  OpenGL_ES_002
+//
+//  Created by GhostClock on 2020/7/26.
+//  Copyright © 2020 GhostClock. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+@end
---
+++ OpenGL ES/OpenGL_ES_002-使用GLKit加载图片/OpenGL_ES_002/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  OpenGL_ES_002
+//
+//  Created by GhostClock on 2020/7/26.
+//  Copyright © 2020 GhostClock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
