d3da6eb71feb97a530bb5128ea9890e0fe3594c1
GhostClock <zhangyuanlaifen@gmail.com>
2020-08-01 22:30:20 +0800

    使用顶点着色器和片元着色器(GLSL)加载图片-图片翻转方法1

---
+++ OpenGL ES/OpenGL_ES_004-使用顶点着色器和片元着色器(GLSL)加载图片-图片翻转1/OpenGL_ES_004/GCView.h
@@ -0,0 +1,17 @@
+//
+//  GCView.h
+//  OpenGL_ES_004
+//
+//  Created by GhostClock on 2020/8/1.
+//  Copyright © 2020 GhostClock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GCView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OpenGL ES/OpenGL_ES_004-使用顶点着色器和片元着色器(GLSL)加载图片-图片翻转1/OpenGL_ES_004/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGL_ES_004
+//
+//  Created by GhostClock on 2020/8/1.
+//  Copyright © 2020 GhostClock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
