b624a0fe8cd50e7881c728a7dedf1552769797e7
GhostClock <zhangyuanlaifen@gmail.com>
2020-08-02 02:56:47 +0800

    使用GLKit绘制三棱锥

---
+++ OpenGL ES/OpenGL_ES_005-使用GLKit绘制三棱锥/OpenGL_ES_005/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  OpenGL_ES_005
+//
+//  Created by GhostClock on 2020/8/2.
+//  Copyright © 2020 GhostClock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ OpenGL ES/OpenGL_ES_005-使用GLKit绘制三棱锥/OpenGL_ES_005/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGL_ES_005
+//
+//  Created by GhostClock on 2020/8/2.
+//  Copyright © 2020 GhostClock. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+
+@end
+
